104204f311e6ae7abb30c3254b4e94b9e9b12a55
dpgc11 <iamyogeshtelang@gmail.com>
2016-12-16 19:28:25 +0530

    finished 46.2

--- 2013-OOProgrammingWithJava-PART2/week12-week12_46.FilmReference/src/reference/Main.java
+++ 2013-OOProgrammingWithJava-PART2/week12-week12_46.FilmReference/src/reference/Main.java
@@ -1,9 +1,16 @@
 package reference;
 
+import reference.domain.Rating;
+
 
 public class Main {
 
     public static void main(String[] args) {
         // Test your code here
+        Rating given = Rating.GOOD;
+        System.out.println("Rating " + given + ", value " + given.getValue());
+        given = Rating.NEUTRAL;
+        System.out.println("Rating " + given + ", value " + given.getValue());
+        
     }
 }
